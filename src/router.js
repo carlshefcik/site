84623ffcbcf224ed2327a5f17746814b2c14bdbf
carlshefcik <32783344+carlshefcik@users.noreply.github.com>
2018-07-14 21:29:25 -0700

    testing

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Items from './components/Items.vue'
+import About from './components/About.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'items',
+      component: Items
+    },
+    {
+      path: '/about/:name',
+      name: 'about',
+      component: About
+    }
+  ]
+})
